406fac78ec4078c8f11855c39d52ed449dd57245
Heather Thacker <hhthacker@gmail.com>
2017-02-09 18:44:04 -0600

    finished exercise

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -1,8 +1,11 @@
 // Create an array that contains the letters of the alphabet
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
 var letterStack= "";
+var letterSpace = "";
+
 // The stackLetter function should accept the array as the sole argument
+// Write a `for` loop that iterates over the array argument and outputs the letters.
+
 function stackLetters (theAlphabetArray) {
 	for (var i = 0; i < alphabet.length; i++) {
 		letterStack += alphabet[i];
@@ -10,13 +13,17 @@ function stackLetters (theAlphabetArray) {
 		}
 	}
 
-
-    /*
-      Write a `for` loop that iterates over the array argument and
-      outputs the letters.
-     */
-
-// Invoke the function and pass in the array
-stackLetters(alphabet)
-
 //if statement to add space after every 3rd letter
+function stackSpaceLetters (theAlphabetArray) {
+for (var i = 0; i < alphabet.length; i++) {
+	if (i%3 === 0) {
+		letterSpace += " ";
+	}
+		letterSpace += alphabet[i];
+		console.log(letterSpace);
+	}
+}
+
+// Invoke the functions and pass in the array
+stackLetters(alphabet);
+stackSpaceLetters(alphabet);
